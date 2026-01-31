7824b2d8ff053363d129d2c0c53b11128dd2a07b
yujink1112 <shirocafe@yahoo.co.jp>
2026-01-31 17:58:58 +0900

    Update send-count.js

--- send-count.js
+++ send-count.js
@@ -2,9 +2,13 @@ import admin from "firebase-admin";
 import axios from "axios";
 
 console.log("B64 length:", process.env.FIREBASE_SERVICE_ACCOUNT_B64.length);
-const decoded = Buffer
-  .from(process.env.FIREBASE_SERVICE_ACCOUNT_B64, "base64")
-  .toString("utf8");
+
+const decoded = JSON.parse(
+  Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_B64,
+    "base64"
+  ).toString("utf8")
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
